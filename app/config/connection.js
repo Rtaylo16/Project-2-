520c128a2c3f5ba4eb3394e2e21678a146512cc1
Ryan Taylor <Superbia@ymail.com>
2020-04-29 19:09:06 -0400

    yuh

--- app/config/connection.js
+++ app/config/connection.js
@@ -1,20 +1,15 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "spot_db"
-});
-
-
-connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-
-  module.exports = connection;
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'Rtaylo16',
+        database: 'spot_db'
+    });
+};
+connection.connect();
+module.exports = connection;
